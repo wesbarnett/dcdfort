1652f26b2c73f58b1ca0d9ad8f5abd0534c7163a
James Barnett <wes@wbarnett.us>
2017-10-23 17:48:44 -0400

    update docs

--- docs/html/search/all_c.js
+++ docs/html/search/all_c.js
@@ -7,7 +7,7 @@ var searchData=
   ['trajectory_5fget_5fbox',['trajectory_get_box',['../namespacedcdfort__trajectory.html#a420c53808653072d326504cc30c6a977',1,'dcdfort_trajectory']]],
   ['trajectory_5fget_5fnatoms',['trajectory_get_natoms',['../namespacedcdfort__trajectory.html#a37a7c3c90c012343c280d1b969701d57',1,'dcdfort_trajectory']]],
   ['trajectory_5fget_5fxyz',['trajectory_get_xyz',['../namespacedcdfort__trajectory.html#a280613c51e6299b81b0b88d545b2e1bd',1,'dcdfort_trajectory']]],
-  ['trajectory_5fopen',['trajectory_open',['../namespacedcdfort__trajectory.html#a44c702cc8e9054e134780e1141ef61d9',1,'dcdfort_trajectory']]],
+  ['trajectory_5fopen',['trajectory_open',['../namespacedcdfort__trajectory.html#ad701a7860faa3c12471d07b8e719c6d3',1,'dcdfort_trajectory']]],
   ['trajectory_5fread',['trajectory_read',['../namespacedcdfort__trajectory.html#af98b3b409501d01657d7cd60c54603e0',1,'dcdfort_trajectory']]],
   ['trajectory_5fread_5fnext',['trajectory_read_next',['../namespacedcdfort__trajectory.html#add404dcb115dc596057d529b28114a59',1,'dcdfort_trajectory']]],
   ['trajectory_5fskip_5fnext',['trajectory_skip_next',['../namespacedcdfort__trajectory.html#ae8c077662b2326c3a897bd08ad5d420f',1,'dcdfort_trajectory']]]
