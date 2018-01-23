4b1ea03d81f8ef72ad2c6eb9e10a7f4420716000
James Barnett <wes@wbarnett.us>
2018-01-23 15:19:17 -0500

    quotes around vars

--- meson/install_modules.sh
+++ meson/install_modules.sh
@@ -3,5 +3,5 @@
 # Installs Fortran module files into include directory
 
 if [ -n "${MESON_INSTALL_DESTDIR_PREFIX}" ]; then
-    install -v -Dm644 ${MESON_BUILD_ROOT}/dcdfort@sha/*.mod -t ${MESON_INSTALL_DESTDIR_PREFIX}/include/
+    install -v -Dm644 "${MESON_BUILD_ROOT}"/dcdfort@sha/*.mod -t "${MESON_INSTALL_DESTDIR_PREFIX}"/include/
 fi
