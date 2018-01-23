f15acb1872482d00c0a5b0db56b92a7b27d66082
James Barnett <wes@wbarnett.us>
2018-01-23 13:39:42 -0500

    protect if empty var

--- meson/install_modules.sh
+++ meson/install_modules.sh
@@ -2,4 +2,6 @@
 
 # Installs Fortran module files into include directory
 
-install -v -Dm644 ${MESON_BUILD_ROOT}/dcdfort@sha/*.mod -t ${MESON_INSTALL_DESTDIR_PREFIX}/include/
+if [ -n "${MESON_INSTALL_DESTDIR_PREFIX}" ]; then
+    install -v -Dm644 ${MESON_BUILD_ROOT}/dcdfort@sha/*.mod -t ${MESON_INSTALL_DESTDIR_PREFIX}/include/
+fi
